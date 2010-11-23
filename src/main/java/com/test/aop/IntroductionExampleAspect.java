2d6c15357ebe2d28fba55b1e5d66a59c9386a7d2
j_urberg <j_urberg@LAP_JURBERG1.eauclaire.namerica.idexxi.com>
2010-11-23 13:27:56 -0600

    Removed unneeded order from aspect

--- src/main/java/com/test/aop/IntroductionExampleAspect.java
+++ src/main/java/com/test/aop/IntroductionExampleAspect.java
@@ -5,23 +5,17 @@ import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.DeclareParents;
-import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class IntroductionExampleAspect implements Ordered {
+public class IntroductionExampleAspect {
 
 	private static final Log log = LogFactory.getLog(IntroductionExampleAspect.class);
 
 	@DeclareParents(value="com.test.trading.*", defaultImpl=DefaultUsageTracked.class)
 	public static UsageTracked mixin;
 	
-	@Override
-	public int getOrder() {
-		return 3;
-	}
-
 	@Before("execution(* com.test.trading.*.*(..)) && this(usageTracked)")
 	public void recordUsage(UsageTracked usageTracked) {
 		usageTracked.incrementUsageCount();
